51293baecb335adafc5f25a9654cf5d51212412f
strannik1105 <danil.stolpovskikh@gmail.com>
2022-12-03 15:44:14 +0700

    Add PlayerC

--- models/Controller.py
+++ models/Controller.py
@@ -1,7 +1,8 @@
 from models.map.PlayerFieldList import PlayerFieldList
 from models.map.map import Map
+from models.players.Player import Player
+from models.players.PlayerDamageable import PlayerDamageable
 from models.players.PlayerAttacker import PlayerAttacker
-from models.players.interfaces.IPlayer import IPlayer
 
 
 class Controller:
@@ -31,6 +32,11 @@ class Controller:
         attack = int(input())
         self.__playerB = PlayerAttacker(render, health, attack)
         self.__map.AddPlayer(self.__playerB)
+        print('PlayerC: ')
+        print('Render: ', end='')
+        render = input()
+        self.__playerC = Player(render)
+        self.__map.AddPlayer(self.__playerC)
         self.__turn = 0
 
     def Play(self):
@@ -40,6 +46,8 @@ class Controller:
                 self.PlayerAMove()
             elif self.__turn == 1:
                 self.PlayerBMove()
+            elif self.__turn == 2:
+                self.PlayerCMove()
             self.__map.PrintMap()
 
     def PlayerAMove(self):
@@ -58,11 +66,11 @@ class Controller:
                 if self.__map.Move(self.__playerA, old_x + x, old_y + y):
                     break
                 else:
-                    # FIX
                     from models.map.fieldMovable import FieldMovable
                     if isinstance(self.__map.GetField(old_x + x, old_y + y), FieldMovable):
-                        self.__playerA.AttackVictim(self.__map.GetField(old_x + x, old_y + y).GetPlayer())
-                        break
+                        if isinstance(self.__map.GetField(old_x + x, old_y + y).GetPlayer(), PlayerDamageable):
+                            self.__playerA.AttackVictim(self.__map.GetField(old_x + x, old_y + y).GetPlayer())
+                            break
                     print('Incorrect turn')
             else:
                 print('Incorrect turn')
@@ -85,11 +93,34 @@ class Controller:
                 if self.__map.Move(self.__playerB, old_x + x, old_y + y):
                     break
                 else:
-                    #FIX
                     from models.map.fieldMovable import FieldMovable
                     if isinstance(self.__map.GetField(old_x + x, old_y + y), FieldMovable):
                         if ((abs(x) == 1) and (abs(y) == 0)) or ((abs(x) == 0) and (abs(y) == 1)):
-                            self.__playerB.AttackVictim(self.__map.GetField(old_x + x, old_y + y).GetPlayer())
+                            if(isinstance(self.__map.GetField(old_x + x, old_y + y).GetPlayer(), PlayerDamageable)):
+                                self.__playerB.AttackVictim(self.__map.GetField(old_x + x, old_y + y).GetPlayer())
+                                break
+                    print('Incorrect turn')
+            else:
+                print('Incorrect turn')
+        print()
+        self.__turn = 2
+
+    def PlayerCMove(self):
+        print('PlayerC turn')
+        print('Your position: ', end='')
+        old_x = PlayerFieldList.get(self.__playerC).x
+        print(old_x, end=' ')
+        old_y = PlayerFieldList.get(self.__playerC).y
+        print(old_y)
+        while True:
+            print('Input x change(ex: 1; 0; -1): ', end=' ')
+            x = int(input())
+            print('Input y change(ex: 1; 0; -1): ', end=' ')
+            y = int(input())
+            if ((abs(x) == 1) and (abs(y) <= 1)) or ((abs(x) <= 1) and (abs(y) == 1)):
+                if self.__map.Move(self.__playerC, old_x + x, old_y + y):
+                    break
+                else:
                     print('Incorrect turn')
             else:
                 print('Incorrect turn')
